46e023b4ad1ab47fc655e8851a48d1fda0d9681b
ShernOC <sherlynea1994@hotmail.com>
2024-11-04 18:13:33 +0300

    got fetch

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+// get the main element
+const main = document.querySelector('main')
+main.style.backgroundImage = " url('images/got.png')";
+main.style.backgroundRepeat = "no-repeat"; 
+main.style.backgroundSize = "100%"
+
+
+
+
+// call the fetch function 
 function fetchBooks(){
   // To pass the tests, don't forget to return your fetch!
- return fetch('https://anapioficeandfire.com/api/books')
+  fetch('https://anapioficeandfire.com/api/books')
   .then((res) => res.json())
   .then((data) => renderBooks(data));
 }
 
+
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `<main>
+    <h2>${book.name}</h2>
+    <p><span> Total pages: ${book.numberOfPages}</span>
+    <br>
+    <span>Author: ${book.authors}</span>
+    <main>`
+    newDiv.style.color= "beige"
+    main.appendChild(newDiv);
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  fetchBooks();
+ fetchBooks();
+
 });
